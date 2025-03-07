ddbe07122bcdd5a1d5011dd3dcd0acea58401182
Sharlene Cera <cera.sharlene@gmail.com>
2025-03-07 20:18:22 +0000

    LevelMenu: Added a way to automatically populate the buttons array depending on the Level Buttons object where all the level buttons are stored.

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 public class LevelMenu : MonoBehaviour
 {
     public Button[] buttons;
+    public GameObject levelButtons;
 
     private void Awake()
     {
+        ButtonsToArray();
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
         for (int i = 0; i < buttons.Length; i++)
         {
@@ -18,6 +20,16 @@ public class LevelMenu : MonoBehaviour
     {
         string levelName = "Level " + levelID;
         SceneManager.LoadScene(levelName);
+    }
+
+    void ButtonsToArray()
+    {
+        int childCount = levelButtons.transform.childCount;
+        buttons = new Button[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
