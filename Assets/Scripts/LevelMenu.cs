95561dcdc7b43ed481c41225ed20b29b6acd5fec
Sharlene Cera <cera.sharlene@gmail.com>
2025-03-07 20:01:10 +0000

    Made all level buttons interactable apart from level 1.

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class LevelMenu : MonoBehaviour
 {
+    public Button[] buttons;
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = i < unlockedLevel;
+        }
+    }
+
+    public void OpenLevel(int levelID)
+    {
+        string levelName = "Level " + levelID;
+        SceneManager.LoadScene(levelName);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,11 +30,5 @@ public class LevelMenu : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void OpenLevel(int levelID)
-    {
-        string levelName = "Level " + levelID;
-        SceneManager.LoadScene(levelName);
     }
 }
